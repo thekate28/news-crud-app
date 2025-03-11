b5ba163cf32a6081372a595599c44740b4004ab5
Kate Zakharova <katezaharova128@gmail.com>
2025-03-11 15:12:03 +0400

    feat/master - update base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from "node:path";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/news-crud-app/',
   resolve: {
     alias: [
       {find: '@', replacement: path.resolve(__dirname, 'src')},
